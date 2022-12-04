389757a9062e195ddca2f5361c7e5ad4f7154732
Ivan Kozlov <103379782+RidiculousCircumstances@users.noreply.github.com>
2022-12-04 20:11:44 +0700

    base case

---
+++ gulpfile.js
@@ -0,0 +1,57 @@
+import gulp from 'gulp';
+import gsass from 'gulp-sass';
+import csso from 'gulp-csso';
+import include from 'gulp-file-include';
+import htmlmin from 'gulp-htmlmin';
+import nodeSass from 'node-sass';
+import { deleteAsync } from 'del';
+import sync from 'browser-sync';
+import ap from 'gulp-autoprefixer';
+import concat from 'gulp-concat';
+
+const sass = gsass(nodeSass);
+ 
+function html () {
+	return gulp.src('src/**.html')
+	.pipe(include({
+		prefix: '@@'
+	}))
+	.pipe(htmlmin({
+		collapseWhitespace: true
+	}))
+	.pipe(gulp.dest('dist'));
+}
+
+function scss () {
+	return gulp.src('src/scss/**.scss')
+		.pipe(sass())
+		.pipe(ap({
+			browsers: ['last 2 versions']
+		}))
+		.pipe(csso())
+		.pipe(concat('index.css'))
+		.pipe(gulp.dest('dist'));
+}
+
+async function clear () {
+	return await deleteAsync('dist');
+}
+
+function serve () {
+	sync.init({
+		server: './dist'
+	 });
+
+	gulp.watch('src/**.html', gulp.series(html)).on('change', sync.reload);
+	gulp.watch('src/scss/**.scss', gulp.series(scss)).on('change', sync.reload);
+}
+
+export async function build () {
+	const task = gulp.series( clear, scss, html);
+	await task();
+}
+
+export async function start () {
+	const task = gulp.series(clear, scss, html, serve);
+	await task();
+}
